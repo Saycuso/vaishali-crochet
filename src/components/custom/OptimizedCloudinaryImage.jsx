161a51341889637eb5318506affdd180031e6e58
Abhishek <abhikhairnar440@gmail.com>
2026-01-28 10:08:34 +0530

    image optimization

--- src/components/custom/OptimizedCloudinaryImage.jsx
+++ src/components/custom/OptimizedCloudinaryImage.jsx
@@ -5,25 +5,23 @@ const OptimizedCloudinaryImage = ({
   src,
   alt,
   className,
-  width = "400",
-  height = "300",
-  // 👇 1. Default sizes (Full width on mobile, 300px on desktop)
-  sizes = "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 300px" 
+  width = "368",
+  height = "368",
+  sizes = "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 368px",
 }) => {
   const src360 = getOptimizedImage(src, 360);
+  const src400 = getOptimizedImage(src, 400);
   const src720 = getOptimizedImage(src, 720);
-  const src1080 = getOptimizedImage(src, 1080);
 
   return (
     <img
-      src={src720}
+      src={src400}
       srcSet={`
         ${src360} 360w,
-        ${src720} 720w,
-        ${src1080} 1080w
+        ${src400} 400w,
+        ${src720} 720w
       `}
-      // 👇 2. Pass the prop to the img tag
-      sizes={sizes} 
+      sizes={sizes}
       alt={alt}
       width={width}
       height={height}
